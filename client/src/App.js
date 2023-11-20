f65bfb6d0e242c17b9f247eca76d29d31fd92210
rahmadh <rahmadhaouadi7@gmail.com>
2023-11-20 11:36:25 +0100

    client update

--- client/src/App.js
+++ client/src/App.js
@@ -1,106 +1,11 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import AdminLogin from "./pages/AdminLogin";
-import UserProfile from "./pages/MyBlogs";
-import AdminDdhboard from "./pages/AdminDashboard";
-import Blogs from "./pages/Blogs";
-import SingleBlog from "./pages/SingleBlog";
-import PrivateRoute from "./Routes/PrivateRoute";
-import AdminRoute from "./Routes/AdminRoute";
-import PublicRoute from "./Routes/PublicRoute";
-import ErrorPage from "./pages/ErrorPage";
-import AddBlog from "./pages/AddBlog";
-import AdminBlogs from "./pages/AdminBlogs";
-import AdminUsers from "./pages/AdminUsers";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/login/admin"
-          element={
-            <PublicRoute>
-              <AdminLogin />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/myBlogs/:id"
-          element={
-            <PrivateRoute>
-              <UserProfile />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <AdminRoute>
-              <AdminDdhboard />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin/blogs"
-          element={
-            <AdminRoute>
-              <AdminBlogs />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin/users"
-          element={
-            <AdminRoute>
-              <AdminUsers />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/blogs"
-          element={
-            <PrivateRoute>
-              <Blogs />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/blogs/:id"
-          element={
-            <PrivateRoute>
-              <SingleBlog />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/addBlog"
-          element={
-            <PrivateRoute>
-              <AddBlog />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/error" element={<ErrorPage />} />
       </Routes>
     </div>
   );
